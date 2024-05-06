06e709df6660aa917ecf632389f4026ad9d055e5
Seojin Jung <83979301+seojinjung@users.noreply.github.com>
2024-05-05 20:02:16 -0400

    move fountain into folder

---
+++ dna_fountain/setup.py
@@ -0,0 +1,13 @@
+"""
+Copyright (C) 2016 Yaniv Erlich
+License: GPLv3-or-later. See COPYING file for details.
+"""
+#all .pyx files in a folder
+# from distutils.core import setup
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+  name = 'MyProject',
+  ext_modules = cythonize(["*.pyx"]),
+)
